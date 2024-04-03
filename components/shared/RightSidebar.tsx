4d0d74d4e0316906399f40db7fbb17bb0a5d8d38
Mohammad Talim <talimmohammad116@gmail.com>
2024-04-03 22:39:00 +0530

    Add the Popular tags section in the RightSidebar

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import RenderTag from "./RenderTag";
 
 /**
  * Dummy data
@@ -14,6 +15,14 @@ const topQuestions = [
       "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
   },
   { _id: 5, title: "Redux Toolkit Not Updating State as Expected" },
+];
+
+const popularTags = [
+  { _id: 1, name: "javascript", totalQuestions: 5 },
+  { _id: 2, name: "react", totalQuestions: 5 },
+  { _id: 3, name: "next", totalQuestions: 5 },
+  { _id: 4, name: "vue", totalQuestions: 2 },
+  { _id: 5, name: "redux", totalQuestions: 10 },
 ];
 
 const RightSidebar = () => {
@@ -39,6 +48,21 @@ const RightSidebar = () => {
                 alt="Chevron right"
               />
             </Link>
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-16">
+        <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+        <div className="mt-7 flex flex-col gap-4">
+          {popularTags.map((tag) => (
+            <RenderTag
+              key={tag._id}
+              _id={tag._id}
+              name={tag.name}
+              totalQuestions={tag.totalQuestions}
+              showCount
+            />
           ))}
         </div>
       </div>
