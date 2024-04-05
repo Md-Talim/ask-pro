ce85941f697a6b6c571cee3938bdfd2be6a3fd3e
Mohammad Talim <talimmohammad116@gmail.com>
2024-04-05 21:50:44 +0530

    Fix: Show the Login and Signup options if SignedOut

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,6 +1,7 @@
 import NavigationLink from "@/components/shared/NavigationLink";
 import { Button } from "@/components/ui/button";
 import { sidebarLinks } from "@/constants";
+import { SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -13,32 +14,34 @@ const LeftSidebar = () => {
         ))}
       </nav>
 
-      <div className="flex flex-col gap-y-3">
-        <Link href="/sign-in">
-          <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
-            <Image
-              src="/assets/icons/account.svg"
-              height={24}
-              width={24}
-              alt="Login"
-              className="invert-colors lg:hidden"
-            />
-            <span className="primary-text-gradient max-lg:hidden">Login</span>
-          </Button>
-        </Link>
-        <Link href="/sign-up">
-          <Button className="small-medium btn-tertiary text-dark400_light900 light-border-2 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
-            <Image
-              src="/assets/icons/sign-up.svg"
-              height={24}
-              width={24}
-              alt="Sign up"
-              className="invert-colors lg:hidden"
-            />
-            <span className="max-lg:hidden">Signup</span>
-          </Button>
-        </Link>
-      </div>
+      <SignedOut>
+        <div className="flex flex-col gap-y-3">
+          <Link href="/sign-in">
+            <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+              <Image
+                src="/assets/icons/account.svg"
+                height={24}
+                width={24}
+                alt="Login"
+                className="invert-colors lg:hidden"
+              />
+              <span className="primary-text-gradient max-lg:hidden">Login</span>
+            </Button>
+          </Link>
+          <Link href="/sign-up">
+            <Button className="small-medium btn-tertiary text-dark400_light900 light-border-2 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+              <Image
+                src="/assets/icons/sign-up.svg"
+                height={24}
+                width={24}
+                alt="Sign up"
+                className="invert-colors lg:hidden"
+              />
+              <span className="max-lg:hidden">Signup</span>
+            </Button>
+          </Link>
+        </div>
+      </SignedOut>
     </section>
   );
 };
