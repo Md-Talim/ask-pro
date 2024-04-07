e8e06eaca0595c5aa9441005d0e96a5e510fe24d
Mohammad Talim <talimmohammad116@gmail.com>
2024-04-07 22:13:41 +0530

    Build basic layout for the home route

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,26 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const HomePage = () => {
-  return <main>Main Feed Section</main>;
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse md:flex-row justify-between">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Link href="/ask-question">
+          <Button className="primary-gradient px-4 py-2 !text-light-900">
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <div>LocalSearchBar</div>
+
+        <div>Filters</div>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
