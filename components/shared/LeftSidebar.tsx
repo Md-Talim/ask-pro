e955e2f59e2c07b8eb2e2494d2824169f999d0a6
Mohammad Talim <talimmohammad116@gmail.com>
2024-04-09 21:57:05 +0530

    Fix: Add key to the NavigationLink component

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -10,8 +10,7 @@ const LeftSidebar = () => {
     <section className="background-light900_dark200 light-border sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-64 custom-scrollbar">
       <nav className="space-y-6">
         {sidebarLinks.map((link) => (
-          // Add keys here...
-          <NavigationLink {...link} isDesktop />
+          <NavigationLink key={link.label} {...link} isDesktop />
         ))}
       </nav>
 
