e9c1337d409d5d35879f65322d17071d94bb597a
Mohammad Talim <talimmohammad116@gmail.com>
2024-04-13 22:21:35 +0530

    Create the ask-question page

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,11 @@
+const AskQuestionPage = () => {
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
+      <div className="mt-8">{/* TODO: Build a question form */}</div>
+    </div>
+  );
+};
+
+export default AskQuestionPage;
