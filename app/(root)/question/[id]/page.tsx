cd4bf7b4740db71723f5f8b2dc1d71e925f6d6af
Mohammad Talim <talimmohammad116@gmail.com>
2024-05-05 11:31:55 +0530

    display the voting UI on question details page

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -3,6 +3,7 @@ import AllAnswers from "@/components/shared/AllAnswers";
 import Metric from "@/components/shared/Metric";
 import ParseHTML from "@/components/shared/ParseHTML";
 import RenderTag from "@/components/shared/RenderTag";
+import Votes from "@/components/shared/Votes";
 import { getQuestionById } from "@/lib/actions/question.action";
 import { getUserById } from "@/lib/actions/user.action";
 import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
@@ -44,7 +45,17 @@ const QuestionPage = async ({ params }: Props) => {
               {question.author.name}
             </p>
           </Link>
-          <div className="flex justify-end">VOTING</div>
+          <div className="flex justify-end">
+            <Votes
+              type="Question"
+              itemId={question._id}
+              userId={user._id}
+              upvotes={question.upvotes.length}
+              hasUpvoted={question.upvotes.includes(user._id)}
+              downvotes={question.downvotes.length}
+              hasDownvoted={question.downvotes.includes(user._id)}
+            />
+          </div>
         </div>
 
         <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
